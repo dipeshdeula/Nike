65b18d8b7e1bcff60794fc8fdbbf368f287af9aa
deuladipesh94@gmail.com <deuladipesh94@gmail.com>
2024-10-20 14:33:22 +0545

    nike website

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import CustomerReviews from './CustomerReviews';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export{
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+
+}
